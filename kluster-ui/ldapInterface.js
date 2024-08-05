c8489d15f149c06d72b0c40c34b20c6603ae0f38
GiuseppeBnn <giusbon2@gmail.com>
2024-08-05 14:42:42 +0200

    feat: implemented LDAP login interface for students

--- kluster-ui/ldapInterface.js
+++ kluster-ui/ldapInterface.js
@@ -1,9 +1,32 @@
 //da implementare controllo cf in server ldap
-function checkCF(cf) {
-    if (!cf || cf === "") {
-        return false;
-    }
-    return true;
+function checkCfLdap(cf,pw){
+  return new Promise((resolve,reject) => {
+    const ldap = require('ldapjs');
+    const ldapUrl = 'ldap://AD-UNICT-DC1.unict.ad'; // AD server url
+    const ldapUrl2='ldap://AD-UNICT-DC2.unict.ad'
+    const baseDn = 'DC=unict,DC=ad'; 
+    const userDn = `CN=${cf},OU=Studenti,${baseDn}`;
+    const ldap_client = ldap.createClient({
+      url: [ldapUrl,ldapUrl2]
+    });
+    ldap_client.on('error', (err) => {
+      console.error('Errore durante la connessione:', err.message);
+      return reject(err);
+    });
+    // Esegui il bind (autenticazione) dell'utente
+    ldap_client.bind(userDn, pw, (err) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+      // Chiudi la connessione dopo il bind
+      ldap_client.unbind((unbindErr) => {
+        if (unbindErr) {
+          console.error('Errore durante la disconnessione:', unbindErr.message);
+        }
+      });
+    });
+  });
 }
-
-module.exports = {checkCF };
+module.exports = {checkCfLdap};
